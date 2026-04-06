d20cfc0508fd299861eb926fbacc22ef10464f0d
sergio <69440338+Sergio-Bautista@users.noreply.github.com>
2026-04-05 21:27:02 -0500

    delete task implement

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const MongoClient = require('mongodb').MongoClient
 const PORT = process.env.PORT || 7000;
 require('dotenv').config();
 
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
 let db, 
     mongoURL = process.env.db_URL,
     dbName = "TO-DO-APP"
@@ -15,24 +20,21 @@ MongoClient.connect(mongoURL)
     db = client.db(dbName)
 })
 
-
-app.set("view engine", "ejs")
-app.use(express.static("public"))
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-
 app.get("/", (req, res) =>{
-    db.collection('tasks').find().toArray()
-    .then(data =>{
+    const tasks = db.collection("tasks").find().toArray()
+    console.log(tasks)
+    
+    tasks.then(data =>{
         res.render('index.ejs', { info: data})
     })
     .catch(error => console.error(error))
-})
-
+});
 
 app.post("/addTask", (req, res) => {
-    db.collection("tasks").insertOne({task: req.body.task, completed: false})
-    .then( () => {
+    
+    const addTask = db.collection("tasks").insertOne({task: req.body.task, completed: false})
+
+    addTask.then( () => {
         console.log("Task Added!!!");
         res.redirect("/")
     })
@@ -40,12 +42,14 @@ app.post("/addTask", (req, res) => {
 });
 
 app.delete("/deleteTask", (req, res) =>{
-    db.collection("tasks").deleteOne({task:req.body.task})
-    .then(() => {
-        console.log("Task deleted!!!");
-        res.json("Task Deleted")
+    db.collection('tasks').deleteOne({ task: req.body.value })
+
+    .then(result => {
+        console.log("task deleted")
+        res.json("task deleted")
     })
     .catch(error => console.error(error))
+
 })
 
 
