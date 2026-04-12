1a93eed06feea14741b39b56ef5ad2f1a3e6477f
sergio <69440338+Sergio-Bautista@users.noreply.github.com>
2026-04-11 22:43:29 -0500

    added complete task function

--- public/main.js
+++ public/main.js
@@ -1,5 +1,39 @@
+
 // gets the button to delete a task and adds an event listener
 const button = document.querySelectorAll(".deleteButton")
+
+const completedButton = document.querySelectorAll(".completedButton");
+
+Array.from(completedButton).forEach(button => {
+    button.addEventListener('click', taskCompleted)
+})
+
+
+async function taskCompleted(){
+
+    const completed = true
+
+    const taskName = document.querySelector(".taskName").textContent
+
+    try{
+        const response = await fetch('/completeTask', {
+            method: 'put', 
+            headers: {'Content-Type' : 'application/json'}, 
+            body: JSON.stringify({
+                value: completed, 
+                task: taskName
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(error){
+        console.log(error)
+    }
+
+}
+
 
 Array.from(button).forEach(button => {
     button.addEventListener('click', deleteTask)
@@ -8,16 +42,15 @@ Array.from(button).forEach(button => {
 
 // function to delete task
 function deleteTask(){
-    const id = this.previousSibling.textContent
-    const name = this.parentNode.previousElementSibling.textContent
+   const taskName = document.querySelector(".taskName").textContent
+   
     // sends a fetch request to the server in the delete route and specify the method
     fetch('/deleteTask', {
         method: 'delete', 
         headers : {'Content-Type': 'application/json'},
         body: JSON.stringify({
             // sends the value of the input file
-          value: name,
-          taskId: id
+          value: taskName,
         })
     })
     .then(res => {
